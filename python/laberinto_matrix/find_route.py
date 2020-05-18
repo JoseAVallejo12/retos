78365baad8b325a326e23c30a7577529e1eefe1a
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-18 10:14:06 -0500

    update

---
+++ python/laberinto_matrix/find_route.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def find_shortest_route(matrix, [0, 0], [5,5]):
+	
--- python/laberinto_matrix/main.py
+++ python/laberinto_matrix/main.py
@@ -2,18 +2,11 @@
 if __name__ == "__main__":
     from matrices import matrix
     from dir_control import dir
+    from find_route import f
 
-    start = (3,5)
-    fin = (5,5)
+    start = (3, 5)
+    fin = (5, 5)
     mtx = matrix(1)
 
-	'''
-	for n in range(1,4):
-		a = matrix(n)
-		pass
-		for i in a:
-			print(i)
-		print("")
-	'''
     dir_mov = dir(start, fin)
     print(dir_mov)
